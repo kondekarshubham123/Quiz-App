d15688c64b0df3fb092b1ab4787732a4f78d5af2
Shubham Kondekar <kondekarshubham123@gmail.com>
2023-07-12 21:51:45 +0530

    Question MS Template added

---
+++ question-service/src/test/java/com/infy/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
